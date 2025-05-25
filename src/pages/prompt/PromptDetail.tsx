358e428c007de8d426a50c0b8f8db6b6bf4b4e12
0woodev <0woodev@gmail.com>
2025-05-26 01:47:09 +0900

    STYLE - PromptDetail 디자인 변경

--- src/pages/prompt/PromptDetail.tsx
+++ src/pages/prompt/PromptDetail.tsx
@@ -3,6 +3,8 @@ import {api} from "@apis/index";
 import {useNavigate, useParams, useRoutes} from "react-router-dom";
 import {useRecoilState} from "recoil";
 import {basePromptState} from "@state/BasePromptState";
+import styled from "styled-components";
+import {Cancel} from "axios";
 
 
 const PromptVersionList: React.FC = () => {
@@ -45,21 +47,138 @@ const PromptVersionList: React.FC = () => {
             {loading ? (
                 <div>Loading...</div>
             ) : (
-                <>
-                    <div>{JSON.stringify(prompt, null, 4)}</div>
-                    <button onClick={() => navigate(`/prompt/${promptName}`)}>
-                        Back to Prompt Versions
-                    </button>
-                    <button onClick={handlePlaygroundClick}>
-                        Playground
-                    </button>
-                </>
+                <PageLayout>
+                    <PromptName>{prompt.prompt_name}
+                        <PromptVersion>{prompt.version}</PromptVersion>
+                    </PromptName>
+
+                    <Params>
+                        <Param>
+                            <Label>Max Completion Tokens</Label>
+                            <Value>{prompt.params?.max_completion_tokens}</Value>
+                        </Param>
+                        <Param>
+                            <Label>Temperature</Label>
+                            <Value>{prompt.params?.temperature}</Value>
+                        </Param>
+                        <Param>
+                            <Label>Created At</Label>
+                            <Value>{prompt.created_at ? new Date(prompt.created_at).toLocaleString() : "N/A"}</Value>
+                        </Param>
+                    </Params>
+                    <MessageLabel>Messages</MessageLabel>
+                    <Messages>
+                        {prompt.params?.messages.map((message: any, index: number) => (
+                            <Message>
+                                <Role>{message.role}</Role>
+                                <Content>{message.content}</Content>
+                            </Message>
+                        ))}
+                    </Messages>
+                    {/*<div>{JSON.stringify(prompt, null, 4)}</div>*/}
+                    {/*<button onClick={() => navigate(`/prompt/${promptName}`)}>*/}
+                    {/*    Back to Prompt Versions*/}
+                    {/*</button>*/}
+                    {/*<button onClick={handlePlaygroundClick}>*/}
+                    {/*    Playground*/}
+                    {/*</button>*/}
+                </PageLayout>
             )}
 
         </>
     )
 }
 
+const PageLayout = styled.div`
+    width: 100%;
+    height: 100%; /**/
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding: 0.5rem 0;
+    // 페이지가 전체가 차게
+`;
+
+const PromptName = styled.div`
+    font-size: 1.5rem;
+    font-weight: bold;
+    padding-left: 0.5rem;
+    margin-bottom: 1rem;
+`;
+
+const PromptVersion = styled.span`
+    width: 100%;
+    font-size: 1rem;
+    font-weight: 600;
+    padding-left: 0.5rem;
+    color: #555;
+`;
+
+const Params = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    width: 100%;
+`;
+
+const MessageLabel = styled.div`
+    font-weight: bold;
+    color: #333;
+    margin-top: 1rem;
+    margin-bottom: 0.5rem;
+    padding-left: 0.5rem;
+`;
+
+const Param = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem;
+    //background-color: #f9f9f9;
+    border-radius: 4px;
+    width: 100%;
+    height: 3rem;
+`;
+
+const Label = styled.div`
+    width: 30%;
+`;
+
+const Value = styled.div`
+    border: 1px solid #ccc;
+    width: 70%;
+    padding: 0.5rem;
+    border-radius: 1rem;
+`;
+
+const Messages = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    width: 100%;
+`;
+
+const Message = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 100%;
+`;
+
+const Role = styled.div`
+    font-weight: bold;
+    color: #333;
+`;
+
+
+const Content = styled.div`
+    margin-top: 0.5rem;
+    color: #555;
+`;
 
 export default PromptVersionList;
 
