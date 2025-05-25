2a95827c211800124742775876a18abb0635cebd
0woodev <0woodev@gmail.com>
2025-05-26 01:33:18 +0900

    STYLE - PromptVersionList 카드형식으로 디자인 변경

--- src/pages/prompt/PromptVersionList.tsx
+++ src/pages/prompt/PromptVersionList.tsx
@@ -30,29 +30,133 @@ const PromptVersionList: React.FC = () => {
             return;
         }
 
-        setPrompts(res);
+        setPrompts([...res]);
+
+        console.log("PromptVersionList", res);
     }
 
     const handlePromptCardClick = (prompt: any) => {
         navigate(`/prompt/${prompt.prompt_name}/${prompt.version}`);
     }
 
+    const DisplayParams = ({prompt}: any) => {
+        console.log("DisplayParams", prompt);
+        const labels = ["maxTokens", "temperature", "createdAt"];
+        const values = [
+            prompt.params?.max_completion_tokens,
+            prompt.params?.temperature,
+            prompt.created_at ? new Date(prompt.created_at).toLocaleString() : "N/A"
+        ];
+
+        return (
+            <PromptInfoLayout>
+                <PromptCommit>commit : {prompt.commit ?? "TODO 아직 구현 안함 (버젼 변경사항 및 요약 작성할 위치)"}</PromptCommit>
+                <PromptParams>
+                    {labels.map((label, index) => (
+                        <Param>
+                            <Label>{label}</Label>
+                            <Value>{values[index]}</Value>
+                        </Param>
+                    ))}
+                </PromptParams>
+            </PromptInfoLayout>
+        );
+    }
+
     return (
-        <>
-            {prompts.map((prompt) => (
-                <div key={uuidv4()} style={{margin: '20px 0'}} onClick={() => {
-                    handlePromptCardClick(prompt);
+        <PageLayout>
+            <PromptName>{promptName}</PromptName>
+            {prompts.map((p) => (
+                <PromptVersionCard key={uuidv4()} style={{margin: '20px 0'}} onClick={() => {
+                    handlePromptCardClick(p);
                 }}>
-                    <h2>{prompt.prompt_name}</h2>
-                    <h4>{prompt.version}</h4>
-                    <div>{JSON.stringify(prompt, null, 4)}</div>
-                </div>
+                    <PromptVersion>{p.version}</PromptVersion>
+                    <DisplayParams prompt={p}/>
+                </PromptVersionCard>
             ))}
-        </>
+        </PageLayout>
     )
 }
 
+const PageLayout = styled.div`
+    width: 100%;
+    height: 100%; /**/
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding: 0.5rem 0;
+    overflow-y: auto;
+    // 페이지가 전체가 차게
+`;
 
+const PromptVersionCard = styled.div`
+    width: 100%;
+    height: 8rem;
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    margin-bottom: 1rem;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #f0f0f0;
+    }
+`;
+
+const PromptName = styled.h2`
+    margin-bottom: 1rem;
+`;
+
+const PromptVersion = styled.div`
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+    font-size: 1.2rem;
+`;
+
+const PromptInfoLayout = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-around;
+    width: 100%;
+    height: 100%;
+    flex-grow: 1;
+`;
+
+const PromptCommit = styled.div`
+    font-weight: bold;
+    margin-right: 1rem;
+`;
+
+const PromptParams = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    //margin-top: 0.5rem;
+    width: 100%;
+`;
+
+const Param = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-right: 1rem;
+`;
+
+const Label = styled.div`
+    font-weight: bold;
+    margin-bottom: 0.2rem;
+    color: #555;
+`;
+
+const Value = styled.div`
+    //background: gray;
+`;
 
 export default PromptVersionList;
 
